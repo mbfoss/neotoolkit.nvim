bae63f4157b669bbf3361f07c5eede04af110c74
mbfoss <mbfoss@fastmail.com>
2026-07-31 06:40:37 +0200

    Update init.lua

--- tests/init.lua
+++ tests/init.lua
@@ -1,8 +1,19 @@
 local plenary_dir = os.getenv("NVIM_PLENARY_DIR") or "/tmp/plenary.nvim"
-local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
-if is_not_a_directory then
+
+local plugin_file = plenary_dir .. "/plugin/plenary.vim"
+if vim.fn.filereadable(plugin_file) == 0 then
+  if vim.fn.isdirectory(plenary_dir) == 1 then
+    print("removing incomplete plenary clone at " .. plenary_dir)
+    vim.fn.delete(plenary_dir, "rf")
+  end
   print("cloning plenary into " .. plenary_dir)
-  vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
+  local out = vim.fn.system({
+    "git", "clone", "--depth", "1",
+    "https://github.com/nvim-lua/plenary.nvim", plenary_dir,
+  })
+  if vim.v.shell_error ~= 0 or vim.fn.filereadable(plugin_file) == 0 then
+    error("failed to clone plenary into " .. plenary_dir .. "\n" .. out)
+  end
 end
 
 vim.opt.rtp:append(".")
